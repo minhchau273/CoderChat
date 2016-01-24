2590e4b24d2354ab643fcc48f29f52126e90317e
Chau Vo <minhchau273@gmail.com>
2016-01-24 22:39:52 +0700

    [f] Add Inbox page template

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,7 @@
+class MessagesController < ApplicationController
+  before_action :require_login
+  
+  def index
+    
+  end
+end
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,4 @@ Rails.application.config.assets.version = '1.0'
 
 Rails.application.config.assets.precompile += %w( users.scss )
 Rails.application.config.assets.precompile += %w( society.scss )
+Rails.application.config.assets.precompile += %w( messages.scss )
