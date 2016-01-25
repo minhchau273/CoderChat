65d0f05c58f1c092a7ef13716528b8b1fda40353
Chau Vo <minhchau273@gmail.com>
2016-01-26 00:04:06 +0700

    [b] Fix drop-down list’s height

--- app/assets/javascripts/plugins/dropdown.js
+++ app/assets/javascripts/plugins/dropdown.js
@@ -5,18 +5,21 @@ window.setupButton = function() {
             content = document.getElementById('content'),
             button = document.getElementById('button');
 
-            button.onclick = function () {
+        var number_of_children = content.children.length;
+        var content_height = 39 * number_of_children;
 
-                if (dropdown.style.height === "117px") {
-                    dropdown.style.height = "0";
-                    content.style.opacity = "0";
-                } else {
-                    dropdown.style.height = "117px";
-                    dropdown.style.transition = "height 0.5s";
-                    content.style.transition = "opacity 0.5s";
-                    content.style.opacity = "1";
-                }
-            };
+        button.onclick = function () {
+
+            if (dropdown.style.height === content_height + "px") {
+                dropdown.style.height = "0";
+                content.style.opacity = "0";
+            } else {
+                dropdown.style.height = content_height + "px";
+                dropdown.style.transition = "height 0.5s";
+                content.style.transition = "opacity 0.5s";
+                content.style.opacity = "1";
+            }
+        };
     })();
 };
 
